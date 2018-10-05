99a17a6d8f07e4c975ff22f79c21ce95ce622cab
Nhi Nguyen <nhi.nguyen@segment.com>
2018-10-05 14:54:14 -0700

    Add script to download the latest version

---
+++ .buildscript/get-latest-version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Usage: get-latest-version.sh [executable_name]
+# Download the latest release, for the given executable (default to "tester_linux_amd64")
+
+executable=$1
+
+if [[ -z ${executable} ]] ; then
+    executable="tester_linux_amd64"
+fi
+
+latest=$(git tag | tail -1)
+
+wget https://github.com/segmentio/library-e2e-tester/releases/download/${latest}/${executable}
